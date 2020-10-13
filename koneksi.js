4b44641cdb749e5843cbf130edf584e27b551307
adzanidham <adzani234@gmail.com>
2020-10-13 14:23:35 +0700

    membuat file koneksi.js

---
+++ koneksi.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql')
+
+// buat koneksi database
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'db_mhs'
+})
+
+conn.connect(err => {
+  if (err) throw err
+  console.log('Mysql Connected')
+})
+
+module.exports = conn
